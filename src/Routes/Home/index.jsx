7eb0b1b4377e8e415b1393b65b23b2efda0e183d
tatiana ortiz <tatianaortiz419@gmail.com>
2024-02-19 18:40:37 -0500

    iniciacion proyecto react

---
+++ src/Routes/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+const Home = () => {
+
+  return (
+    <main className="containerHome">
+      <h1>Buscador de productos</h1>
+      
+    </main>
+  )
+}
+
+export default Home
