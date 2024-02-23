4088afeb6564af9a3661fd4bdf26a9aab76384e9
Juan Mercado <juan.mercado1986@gmail.com>
2024-02-22 23:10:47 -0300

    Reingenieria de carpetas y se agrego las rutas de la pagina

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './componentes/App.jsx'
+import { BrowserRouter } from "react-router-dom";
 
+import { App } from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { Home } from '../pages/Home';
+import { Admin } from '../pages/Admin';
+import { AgregarProducto } from '../pages/AgregarProducto';
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        <Route path="/home" element={ <Home  /> } />
+        <Route path="/" element={ <Navigate to="/home" /> } />
+        <Route path="/admin" element={ <Admin /> } />
+        <Route path="/AgregarProducto" element={ <AgregarProducto /> } />
+    </Routes>
+  )
+}
