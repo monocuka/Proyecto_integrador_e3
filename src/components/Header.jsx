bbf6b826b9602f6c92d0e03e9459d5f2f4f8cf61
Juan Mercado <juan.mercado1986@gmail.com>
2024-02-23 16:00:59 -0300

    Header en React finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,33 +2,55 @@ import { useState, useEffect } from 'react';
 
 import '../assets/css/header.css'
 import logo from '../assets/img/logo-sin-fondo-ni letras.png';
+import logoMobile from '../assets/img/soloLogoPng.png';
 import menuHamburguesa from '../assets/img/burger-menu.svg';
 import menuCerrar from '../assets/img/close.svg';
 
 export const Header = () => {
-  const [isBurger, setIsBurger] = useState(true);
-//   const [fadeout, setFadeout] = useState(false);
+    const [isBurger, setIsBurger] = useState(true);
+    const [isLogoMobile, setLogoMobile] = useState( ' ' );
+    const [colorMenu, setColorMenu] = useState( '#F1DFBE' );
+    const [displayLogo, setDisplayLogo] = useState( 'block' );
+    const [navigationMobile, setNavigationMovile] = useState('');
+    
+    const toggleMenu = () => setIsBurger(!isBurger);
 
-  useEffect(() => {
-    console.log(isBurger);
-    // if (!isBurger) {
-    //   setFadeout(true);
-    //   const timer = setTimeout(() => {
-    //     setFadeout(false);
-    //   }, 300);
-    //   return () => clearTimeout(timer);
-    // }
-}, [isBurger]);
+    useEffect(() => {
+        
+        const cambiarLogo = () => setLogoMobile(window.innerWidth < 690 ? logoMobile : logo );
 
-  const toggleMenu = () => setIsBurger(!isBurger);
+        cambiarLogo();
+    
+        window.addEventListener('load', cambiarLogo);
+        window.addEventListener('resize', cambiarLogo);
+    
+        return () => {
+          window.removeEventListener('resize', cambiarLogo);
+          window.removeEventListener('load', cambiarLogo);
+        };
+
+    }, []); 
+
+    useEffect(() => {
+        if ( isBurger ) {
+            setColorMenu( '#F1DFBE' );
+            setDisplayLogo( 'block' );
+            setNavigationMovile('navigation--mobile--fadeout');
+        } else {
+            setColorMenu( '#342822' );
+            setDisplayLogo( 'none' );
+            setNavigationMovile('navigation--mobile');
+        }
+    }, [isBurger]);
+
 
     return(
         <div className="menu__wrapper">
-        <div className="menu__bar">
+        <div className="menu__bar" style={{'backgroundColor': colorMenu}}>
             <div>
                 <div className="header-left">    
                     <a href="#" title="Logo" className="logo">
-                        <img className="logo-header" src={ logo } alt="logo" />
+                        <img className="logo-header" src={ isLogoMobile } style={{'display' : displayLogo}} alt="logo" />
                     </a>
                     <div className="texto-logo">
                         <h3>Construye sin límites</h3>
@@ -36,13 +58,13 @@ export const Header = () => {
                     </div>
                 </div>
             </div>
-            <img className="menu-icon" src={ menuHamburguesa } title='Burger Menu' alt='Burger Menu'
+            <img className="menu-icon" src={ (isBurger) ? menuHamburguesa : menuCerrar } title='Burger Menu' alt='Burger Menu'
                 onClick={ toggleMenu } />
             <div className="button-header">
                 <button className="button-iniciar">Inciar sesión</button>
                 <button className="button-registrate">Regístrarse</button>
             </div>
-            <ul className="navigation">
+            <ul className={`navigation ${navigationMobile}`}>
                 <h2>Menú</h2>
                 <li>
                     <a href="#login" title="login">
@@ -58,39 +80,4 @@ export const Header = () => {
         </div>
     </div>
     );
-//   return (
-//     <div className="menu__wrapper">
-//         <div className="menu__bar" style={{backgroundColor: isBurger ? '#F1DFBE' : '#342822'}}>
-//             <div className="header-left" >    
-//                 <a href="#" title="Logo" className="logo">
-//                     <div className="caja-logo" style={{display: isBurger ? 'flex' : 'none'}}>
-//                         <img className="logo-header" src={ logo } alt="logo" />
-//                     </div>
-//                 </a>
-//                 <div className="texto-logo">
-//                     <h3>Construye sin límites</h3>
-//                     <h5>alquila con facilidad</h5>
-//                 </div>
-//             </div>
-//             <img className="menu-icon" src={ isBurger ? menuHamburguesa : menuCerrar } title='Burger Menu' alt='Burger Menu' onClick={toggleMenu} />
-//             <div className="button-header">
-//                 <button className="button-iniciar">Inciar sesión</button>
-//                 <button className="button-registrate">Regístrarse</button>
-//             </div>
-//             <ul className={`navigation ${isBurger ? '' : 'navigation--mobile'} ${fadeout ? 'navigation--mobile--fadeout' : ''}`}>
-//                 <h2>Menú</h2>
-//                 <li>
-//                     <a href="#login" title="sesion">
-//                         Iniciar Sesión
-//                     </a>
-//                 </li>
-//                 <li>
-//                     <a href="#registry" title="registrarse">
-//                         Regístrarse
-//                     </a>
-//                 </li>
-//             </ul>
-//         </div>
-//     </div>
-//   );
 }
