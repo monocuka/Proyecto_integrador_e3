33a75263150240a3866b64f01d50feda2e1e3425
tatiana ortiz <tatianaortiz419@gmail.com>
2024-02-28 13:04:11 -0500

    renderice los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { AppRouter } from './router/AppRouter';
 import './App.css'
+import Buscador from 'components/Buscador'
+import Categorias from './components/Categorias';
 
 export const App = () => {
   return (
     <>
       <AppRouter />
+      <Buscador />
+      <Categorias />
     </>
   )
 }
+
