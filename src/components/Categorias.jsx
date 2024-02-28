0d827929d40c69807da1727029f19b6b750fe26a
tatiana ortiz <tatianaortiz419@gmail.com>
2024-02-28 13:03:06 -0500

    empezando componentes

---
+++ src/components/Categorias.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Categorias = () => {
+    return (
+        <div className="categorias">
+            <select>
+                <option value="">Seleccione una categoría</option>
+            </select>
+        </div>
+    );
+};
+
+export default Categorias;
