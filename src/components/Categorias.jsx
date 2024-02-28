b421a69ac861eb2ec02002cc99bf31263d5b8aee
tatiana ortiz <tatianaortiz419@gmail.com>
2024-02-28 17:06:27 -0500

    terminado lista

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const Categorias = () => {
+const Categorias = ({ onSelectCategoria }) => {
+    const categorias = [
+        { id: 1, nombre: 'Categoria 1' },
+        { id: 2, nombre: 'Categoria 2' },
+        { id: 3, nombre: 'Categoria 3' },
+        { id: 4, nombre: 'Categoria 4' },
+        { id: 5, nombre: 'Categoria 5' },
+        // Puedes agregar más categorías según sea necesario
+    ];
+
     return (
         <div className="categorias">
-            <select>
+            <select className="lista categorias" onChange={(e) => onSelectCategoria(e.target.value)}>
                 <option value="">Seleccione una categoría</option>
+                {categorias.map((categoria) => (
+                    <option key={categoria.id} value={categoria.id}>
+                        {categoria.nombre}
+                    </option>
+                ))}
             </select>
         </div>
     );
