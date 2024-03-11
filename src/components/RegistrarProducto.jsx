0e425bb4e2b71eecc204df3aee010eec1595afde
Jorge Corrales <jorgecorralesdh@gmail.com>
2024-03-10 22:34:18 -0500

    Registrar Producto JorgeC

--- src/components/RegistrarProducto.jsx
+++ src/components/RegistrarProducto.jsx
@@ -1,32 +1,30 @@
-import '../assets/css/productoRegistrar.css';
-import herrramienta from '../assets/img/mano-vista-lateral-herramienta-electrica 11.svg';
-
+ import '../assets/css/productoRegistrar.css'
 export const RegistrarProducto = () => {
-    const btnClick = async (event) => {
-        event.preventDefault();
-        var imagen = document.getElementById("product-image").files[0];
-        var name = document.getElementById("product-name").value;
-        var cost = document.getElementById("cost").value;
-        var quantity = document.getElementById("quantity").value;
-        var description = document.getElementById("description").value;
-        var codigo = document.getElementById("code").value;
-    
-        var responseElement = document.getElementById("response");
-    
-        var productData = {
-          nombre: name,
-          descripcion: description,
-          precio: cost,
-          codigo: codigo,
-          cantidad: quantity,
-          categoria: {
+  const btnClick = async (event) => {
+    event.preventDefault();
+    const imagen = document.getElementById("product-image").files[0];
+    const name = document.getElementById("product-name").value;
+    const cost = document.getElementById("cost").value;
+    const quantity = document.getElementById("quantity").value;
+    const description = document.getElementById("description").value;
+    const codigo = document.getElementById("code").value;
+
+    const responseElement = document.getElementById("response");
+
+    const productData = {
+        nombre: name,
+        descripcion: description,
+        precio: cost,
+        codigo: codigo,
+        cantidad: quantity,
+        categoria: {
             cat_id: 1,
             nombre: "Compresores de aire",
             descripcion: "Son ideales para darle potencia a otras herramientas neumáticas o bien realizar múltiples tareas como inflar neumáticos de coches y bicicletas, limpiar o hasta rociar pintura."
-          }
-        };
+        }
+    };
     
-        const url = 'http://localhost:8080/products';
+        const url = 'localhost:8080/api/producto/';
         let formData = new FormData();
         formData.append('producto', JSON.stringify(productData));
         formData.append('imagen', imagen);
@@ -58,61 +56,50 @@ export const RegistrarProducto = () => {
     
       return (
         <div className="registro-container">
-     
-    
           <div className="body-container">
             <div className="company-image">
-              <img src={herrramienta} alt="Imagen de la empresa" />
+            <img className='img-agregar' src='/src/assets/img/foto registrar.png' alt="Imagen de la empresa" />
+            <img className='img-agregar-tablet img-agregar' src='/src/assets/img/foto fondo.png' alt="Imagen de la empresa" />
             </div>
     
             <div className="product-form">
-              <br />
               <h2>Agregar Producto</h2>
-              <form>
-                <label htmlFor="product-image">Imagen del Producto</label>
-                <input type="file" id="product-image" name="product-image" />
+              <form className='form-agregar'>
+                <div className="form-input">
+
+                <label className='name-input' htmlFor="product-image">Imagen del Producto</label>
+                <input className='input-ingreso' type="file" id="product-image" name="product-image" />
     
-                <label htmlFor="product-name">Nombre del Producto</label>
-                <input type="text" id="product-name" name="product-name" />
+                <label className='name-input' htmlFor="product-name">Nombre del Producto</label>
+                <input className='input-ingreso' type="text" id="product-name" name="product-name" />
     
-                <label htmlFor="product-code">Codigo</label>
-                <input type="text" id="code" name="code" />
+                <label className='name-input' htmlFor="product-code">Codigo</label>
+                <input className='input-ingreso' type="text" id="code" name="code" />
     
-                {/* <label htmlFor="category">Categoría</label>
+                {/* <label className='name-input' htmlFor="category">Categoría</label>
                 <select id="category" name="category">
                   { Opciones de categoría }
                 </select> */}
     
-                <label htmlFor="cost">Costo</label>
-                <input type="number" id="cost" name="cost" />
+                <label className='name-input' htmlFor="cost">Costo</label>
+                <input className='input-ingreso' type="number" id="cost" name="cost" />
     
-                <label htmlFor="quantity">Cantidad</label>
-                <input type="number" id="quantity" name="quantity" />
+                <label className='name-input' htmlFor="quantity">Cantidad</label>
+                <input className='input-ingreso' type="number" id="quantity" name="quantity" />
     
-                <label htmlFor="description">Descripción</label>
-                <textarea id="description" name="description"></textarea>
+                <label className='name-input' htmlFor="description">Descripción</label>
+                <textarea className='input-ingreso' id="description" name="description"></textarea>
     
+                </div>
                 <div>
                   <button className='button-custom' onClick={btnClick}>Agregar Producto</button>
-                  <button className='button-custom1' type="button">Cancelar</button>
+                  <button className='button-custom button-custom-cancelar' type="button">Cancelar</button>
                 </div>
     
-                <div>
-                  <button className='button-custom' type="submit">Volver</button>
-                </div>
                 <p id= "response"></p>
               </form>
             </div>
           </div>
-    
-          <footer>
-            <div className="footer-images icon-white">
-              <i className="fa-brands fa-facebook"></i>
-              <i className="fa-brands fa-linkedin"></i>
-              <i className="fa-brands fa-twitter"></i>
-              <i className="fa-brands fa-instagram"></i>
-            </div>
-          </footer>
         </div>
       );
 }
