d9bacfa16699df022a4d626193dda60b94ad280f
Nahuel Curi <curin1050@protonmail..com>
2024-03-14 15:21:07 -0300

    arregle listarproducto y registrar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,9 +7,10 @@ const Card = ({ product }) => {
     if (!product) {
         return null; // O puedes devolver algún componente de carga o un mensaje de error
     }
-
+    console.log("Dentro de la card el producto es: ", product);
+    //console.log("el path de la imagen dentro del card: " + product.imagenes[0].urlImagen);
     // Ruta de la imagen
-    const imagePath = product.imagenes[0].urlImagen;
+    const imagePath = product.imagenes && product.imagenes.length > 0 ? product.imagenes[0].urlImagen : null;
 
     return (
         <div className="Cards">
