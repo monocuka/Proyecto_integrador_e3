addd40e7add804127333382a68861977a2bab9ab
eliana osorio <yasminosorio2013@gmail.com>
2024-03-26 20:59:28 -0500

    fix changes

--- src/components/CardDetalle.jsx
+++ src/components/CardDetalle.jsx
@@ -1,4 +1,4 @@
-import React from 'react';;
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Calendario from '../components/Calendario';
 import BotonReservas from '../components/BotonReservas';
