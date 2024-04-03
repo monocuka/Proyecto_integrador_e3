34dff606c9a4d3e4a043186752b10f13e44128ec
Daniela George jiménez <d47@misena.edu.co>
2024-04-03 17:07:34 -0500

    Se valida si el usuario está o no logueado al hacer click en reservar

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -4,215 +4,72 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import Calendario from './Calendario'; // Importa tu componente de Calendario
 import 'react-calendar/dist/Calendar.css';
-import Swal from 'sweetalert2'; // Importa SweetAlert2
 
 const Buscador = ({ updateProductos }) => {
-    const [nombreBusqueda, setNombreBusqueda] = useState('');
-    const [fechaBusqueda, setFechaBusqueda] = useState('');
-    const [resultados, setResultados] = useState([]);
-    const [fetchStatus, setFetchStatus] = useState('idle');
-    const [mostrarCalendario, setMostrarCalendario] = useState(false); // Estado para controlar la visibilidad del componente de Calendario
-    const node = useRef();
+  const [nombreBusqueda, setNombreBusqueda] = useState('');
+  const [fechaBusqueda, setFechaBusqueda] = useState('');
+  const [resultados, setResultados] = useState([]);
+  const [error, setError] = useState(null);
+  const [fetchStatus, setFetchStatus] = useState('idle');
+  const [mostrarCalendario, setMostrarCalendario] = useState(false); // Estado para controlar la visibilidad del componente de Calendario
+  const node = useRef();
 
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
-    const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
 
-    const handleDateChange = (date) => {
-        const today = new Date(); // Obtener la fecha de hoy
-        const formattedToday = today.toISOString().split('T')[0];
+  const fetchData = async () => {
+    setFetchStatus('loading');
+    try {
+      if(startDate === null || endDate === null) {
+        setError("Debe completar las fechas"); // Guardar el mensaje de error
+        setFetchStatus('error');
+        return;
+      }
 
-        const selectedDate = new Date(date);
-        if (selectedDate < today) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'No se pueden seleccionar fechas anteriores a hoy.'
-            });
-            return;
-        }
-
-        const formattedDate = selectedDate.toISOString().split('T')[0];
-        if (!startDate) {
-            setStartDate(formattedDate);
-        } else if (!endDate) {
-            setEndDate(formattedDate);
-        } else {
-            setStartDate(formattedDate);
-            setEndDate(null);
-        }
-    };
-
-    const toggleCalendario = () => {
-        setMostrarCalendario(!mostrarCalendario); // Cambia el estado de visibilidad del componente de Calendario
-    };
-
-    const handleChange = async (e) => {
-        const value = e.target.value;
-        setNombreBusqueda(value);
-
-        try {
-            const response = await fetch(`http://localhost:8080/api/producto/nombresSimilares/${value}`);
-            const data = await response.json();
-            setFilteredSuggestions(data);
-
-            if (Array.isArray(data)) {
-                setFilteredSuggestions(data.map(item => item.nombre));
-
-            } else {
-                setFilteredSuggestions([]);
-            }
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'Error al buscar nombres similares.'
-            });
-        }
-    };
-
-    const handleClickOutside = (e) => {
-
-        if (node.current && !node.current.contains(e.target)) {
-            // Click outside the component
-            setFilteredSuggestions([]);
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener('click', handleClickOutside);
-
-        return () => {
-            document.removeEventListener('click', handleClickOutside);
-        };
-    }, []);
-
-    const fetchData = async () => {
-<<<<<<< HEAD
-        setFetchStatus('loading');
-        try {
-
-            if (startDate === null || endDate === null) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Debes completar las fechas.'
-                });
-                setFetchStatus('error');
-                return;
-            }
-
-            const start = new Date(startDate);
-            const end = new Date(endDate);
-
-            const differenceInMs = end - start;
-            const differenceInHours = differenceInMs / (1000 * 60 * 60);
-
-            if (differenceInHours < 48) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Debes seleccionar más de 48 horas y fechas correctas.'
-                });
-                setFetchStatus('error');
-                return;
-            }
-
-            const res = await fetch(`http://localhost:8080/api/producto/disponibilidad/fechainicial/${startDate}/fechafinal/${endDate}?busqueda=${nombreBusqueda}`);
-
-            if (!res.ok) {
-                if (res.status === 404 || res.status === 500) {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Producto no encontrado.'
-                    });
-                    setFetchStatus('error');
-                } else {
-                    throw new Error('La solicitud no fue exitosa');
-                }
-            } else {
-                const jsonData = await res.json();
-                if (jsonData.length === 0) {
-                    setResultados([]);
-                } else {
-                    setResultados(jsonData);
-                    updateProductos(jsonData);
-                }
-                setFetchStatus('success');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: error.message
-            });
-=======
-      setFetchStatus('loading');
-      try {
-        
-        if(startDate === null || endDate === null) {
-          setError("Debe completar las fechas"); // Guardar el mensaje de error
+      const res = await fetch(`http://localhost:8080/api/producto/disponibilidad/fechainicial/${startDate}/fechafinal/${endDate}?busqueda=${nombreBusqueda}`);
+      
+      if (!res.ok) {
+        if (res.status === 404 || res.status === 500) {
+          setError('Product not found');
           setFetchStatus('error');
-          return;
+        } else {
+          throw new Error('La solicitud no fue exitosa');
         }
-        console.log
-        
-        const res = await fetch(`http://localhost:8080/api/producto/disponibilidad/fechainicial/${startDate}/fechafinal/${endDate}?busqueda=${nombreBusqueda}`);
-        
-        if (!res.ok) {
-          if (res.status === 404 || res.status === 500) {
-            setError('Product not found');
->>>>>>> cccbefb6a0a700e2870f8fe9fdb4a18b6d8ddab8
-            setFetchStatus('error');
+      } else {
+        const jsonData = await res.json();
+        if (jsonData.length === 0) { // Check if the array is empty
+          setError('No results found');
+        } else {
+          setResultados(jsonData);
+          updateProductos(jsonData);
+          setError(null); // Limpiar error si la solicitud es exitosa
         }
-    };
+        setFetchStatus('success');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setError(error.message); // Guardar el mensaje de error
+      setFetchStatus('error');
+    }
+  };
+  
+  const handleDateChange = (date) => {
+    const formattedDate = date.toISOString().split('T')[0];
+    if (!startDate) {
+      setStartDate(formattedDate);
+    } else if (!endDate) {
+      setEndDate(formattedDate);
+    } else {
+      setStartDate(formattedDate);
+      setEndDate(null);
+    }
+  };
 
-    return (
-        <div className="autocomplete-container" ref={node}>
-            <input
-                className='input-buscador'
-                type="text"
-                placeholder="Escribe el nombre de la maquinaria..."
-                value={nombreBusqueda}
-                onChange={handleChange}
-            />
-            {filteredSuggestions.length > 0 && (
-                <ul className="suggestions-list">
-                    {filteredSuggestions.map((suggestion, index) => (
-                        <li key={index} onClick={() => setNombreBusqueda(suggestion)}>
-                            {suggestion}
-                        </li>
-                    ))}
-                </ul>
-            )}
-            <div className='bucador-inputs'>
-                <input
-                    className='input-buscador'
-                    type="text"
-                    placeholder={`Fecha inicial: ${startDate ? startDate : 'Not selected'} | Fecha Final: ${endDate ? endDate : 'Not selected'}`}
-                    onChange={(e) => setFechaBusqueda(e.target.value)}
-                />
-                <div onClick={toggleCalendario} className='bucador-inputs' style={{ cursor: 'pointer' }} >
-                    <FontAwesomeIcon icon={faCalendarAlt} size="2x" />
-                </div>
-            </div>
-            <button
-                className='btn-buscar'
-                onClick={fetchData}
-            >
-                Buscar
-            </button>
+  const toggleCalendario = () => {
+    setMostrarCalendario(!mostrarCalendario); // Cambia el estado de visibilidad del componente de Calendario
+  };
 
-<<<<<<< HEAD
-            {fetchStatus === 'success' && resultados.length > 0 && <p>Productos encontrados: {resultados.length}</p>}
-            {fetchStatus === 'error' && <p>Error al realizar la búsqueda.</p>}
-            {mostrarCalendario && <Calendario onChange={handleDateChange} />} {/* Muestra el componente de Calendario si mostrarCalendario es true */}
-        </div>
-    );
-=======
   const handleFechaInputChange = (event) => {
     setFechaBusqueda(event.target.value);
   };
@@ -222,18 +79,17 @@ const Buscador = ({ updateProductos }) => {
     setNombreBusqueda(value);
 
     try {
-        const response = await fetch(`http://localhost:8080/api/producto/nombresSimilares/${value}`);
-        const data = await response.json();
-        setFilteredSuggestions(data);
-        
-        if (Array.isArray(data)) {
-          setFilteredSuggestions(data.map(item => item.nombre));
-        
-        } else {
-          setFilteredSuggestions([]);
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
+      const response = await fetch(`http://localhost:8080/api/producto/nombresSimilares/${value}`);
+      const data = await response.json();
+      setFilteredSuggestions(data);
+
+      if (Array.isArray(data)) {
+        setFilteredSuggestions(data.map(item => item.nombre));
+      } else {
+        setFilteredSuggestions([]);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
   };
 
@@ -243,7 +99,6 @@ const Buscador = ({ updateProductos }) => {
   };
 
   const handleClickOutside = (e) => {
-    
     if (node.current && !node.current.contains(e.target)) {
       // Click outside the component
       setFilteredSuggestions([]);
@@ -254,12 +109,12 @@ const Buscador = ({ updateProductos }) => {
     document.addEventListener('click', handleClickOutside);
 
     return () => {
-        document.removeEventListener('click', handleClickOutside);
+      document.removeEventListener('click', handleClickOutside);
     };
   }, []);
 
   return (
-    <div className="autocomplete-container"  ref={node}>
+    <div className="autocomplete-container" ref={node}>
       <div className='container-inputs'>
         <input 
           className='input-buscador' 
@@ -269,13 +124,13 @@ const Buscador = ({ updateProductos }) => {
           onChange={handleChange}
         />
         {filteredSuggestions.length > 0 && (
-            <ul className="suggestions-list">
-                {filteredSuggestions.map((suggestion, index) => (
-                    <li key={index} onClick={() => handleSelect(suggestion)}>
-                        {suggestion}
-                    </li>
-                ))}
-            </ul>
+          <ul className="suggestions-list">
+            {filteredSuggestions.map((suggestion, index) => (
+              <li key={index} onClick={() => handleSelect(suggestion)}>
+                {suggestion}
+              </li>
+            ))}
+          </ul>
         )}
         <div className='bucador-inputs'>
           <input 
@@ -290,7 +145,7 @@ const Buscador = ({ updateProductos }) => {
           <button className='btn-buscar' onClick={fetchData}>
             Buscar
           </button>
-      </div>
+        </div>
       </div>
 
       {fetchStatus === 'success' && resultados != null && resultados.length > 0 && <p>Productos encontrados: {resultados.length}</p>}
@@ -298,7 +153,6 @@ const Buscador = ({ updateProductos }) => {
       {mostrarCalendario && <Calendario onChange={handleDateChange} />} {/* Muestra el componente de Calendario si mostrarCalendario es true */}
     </div>
   );
->>>>>>> cccbefb6a0a700e2870f8fe9fdb4a18b6d8ddab8
 };
 
 export default Buscador;
--- src/components/Calendario.jsx
+++ src/components/Calendario.jsx
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import '../assets/css/Calendario.css';
 
-
-//import 'react-calendar/dist/Calendar.css';
-import '../assets/css/Calendario.css';
-
 const Calendario = ({ reserva, onChange }) => {
-    const [date, setDate] = useState(new Date());
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
 
-    const siguienteMes = new Date(date);
+    const siguienteMes = new Date(startDate);
     siguienteMes.setMonth(siguienteMes.getMonth() + 1);
+
+    const handleStartDateChange = (date) => {
+        setStartDate(date);
+    };
+
+    const handleEndDateChange = (date) => {
+        setEndDate(date);
+    };
 
     const marcarFechasSeleccionadas = ({ date }) => {
         if (reserva) {
@@ -26,51 +31,33 @@ const Calendario = ({ reserva, onChange }) => {
         }
     };
 
-
-    const marcarFechasSeleccionadas1 = ({ date }) => {
-        if (reserva) {
-            for (let i = 0; i < reserva.length; i++) {
-                const fecha_desde = new Date(reserva[i].fecha_desde[0], reserva[i].fecha_desde[1] - 1, reserva[i].fecha_desde[2]);
-                const fecha_hasta = new Date(reserva[i].fecha_hasta[0], reserva[i].fecha_hasta[1] - 1, reserva[i].fecha_hasta[2]);
-
-                if (date.getMonth() === fecha_desde.getMonth() && date.getFullYear() === fecha_desde.getFullYear()) {
-                    if (date.getDate() >= fecha_desde.getDate() && date.getDate() <= fecha_hasta.getDate()) {
-                        return <div className="selected-date">❌</div>;
-                    }
-                }
-            }
-        }
-    };
-
     return (
         <div className="cal-container">
             <div className="title-container">
                 <h2>Calendario</h2>
-                <div></div>
-                <h1></h1>
             </div>
             <div className="mes">
-                <h3>{nombreMes(date.getMonth())} {date.getFullYear()}</h3>
+                <h3>{nombreMes(startDate.getMonth())} {startDate.getFullYear()}</h3>
                 <Calendar
-                    onChange={onChange}
-                    value={date}
+                    onChange={handleStartDateChange}
+                    value={startDate}
                     calendarType="gregory"
                     showNavigation={true}
                     tileContent={marcarFechasSeleccionadas}
                     minDetail="year"
-                    minDate={new  Date()}
+                    minDate={new Date()}
                 />
             </div>
             <div className="mes">
                 <h3>{nombreMes(siguienteMes.getMonth())} {siguienteMes.getFullYear()}</h3>
                 <Calendar
-                    onChange={onChange}
-                    value={siguienteMes}
+                    onChange={handleEndDateChange}
+                    value={endDate}
                     calendarType="gregory"
                     showNavigation={true}
-                    tileContent={marcarFechasSeleccionadas1}
+                    tileContent={marcarFechasSeleccionadas}
                     minDetail="year"
-                    minDate={new  Date()}
+                    minDate={new Date()}
                 />
             </div>
         </div>
